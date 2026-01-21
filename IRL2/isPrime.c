25bb6ddbb32c439c663771e7ab2f998ab9a4ff96
Hextery <qyntar@gmail.com>
2026-01-21 11:08:50 +0100

    .

--- IRL2/isPrime.c
+++ IRL2/isPrime.c
@@ -1,23 +1,44 @@
-#include <stdio.h>
-#include <omp.h>
+#include <iostream>
+#include <vector>
+#include <utility>
 
-int N = 50000000;
+int main() {
+    const int n = 50 000 000; // n = max
+    std::vector<std::pair<int, bool>> myVector;
+    for (int i = 1; i <= n; ++i) {
+        myVector.emplace_back(i, true);
+    }
 
-//If is prime, returns 1; else returns 0
-int isPrime(int n) {
-    //Fill me!
-    return 1;
-}
+    //int k = 2; // b�rjar p� tv�
+    for (int k = 2; k < n ; k++)
+    {
+        if (myVector[k].second) {
+            // kollar prim
+            for (int i = k; i <= n; i += k) {
+                myVector[i - 1].second = false;
+            }
+        }
+    }/*
+    while (k * k <= n) {
+        if (myVector[k - 1].second) {
+            // kollar prim
+            for (int i = k; i <= n; i += k) {
+                myVector[i - 1].second = false;
+            }
+        }
+        k++;
+    }*/
 
-int main()
-{
-    int countPrimes = 0;
-    double begin,end;
+    // Skriver ut
+    std::cout << "Prime numbers up to " << n << ":" << std::endl;
+    int count = 0;
+    for (const auto& pair : myVector) {
+        if (pair.second) { 
+            //std::cout << pair.first << " ";
+            count++;
+        }
+    }
+    std::cout << "Amount : " << count << std::endl;
 
-    //Task: find the total number of prime numbers in the range [2, N]
-    begin = omp_get_wtime();
-    //Fill me! (Call isPrime)
-    end = omp_get_wtime();
-    printf("Found %d prime numbers between 1 and %d\n", countPrimes, N);
-    printf("Took %lfs to find primes\n", end-begin);
+    return 0;
 }
---
+++ IRL2/p_thread.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <pthread.h>
+
+
+void *worker(){}
+
+int main () {
+    int N = 512 * 1024 * 1024;
+    int max_n = 22;
+    int *A, *B;
+    long dot_product = 0;
+    double begin,end; // Timestamps for beginning and end of computation
+
+    //Allocate Array A and B with N elements
+    //Fill in
+    A = (int*) malloc((size_t)N * sizeof(int));
+    B = (int*) malloc((size_t)N * sizeof(int));
+    for (int i = 0; i < N; ++i) {
+        A[i] =  rand() %  max_n; /* change as desired */
+        B[i] = rand() %  max_n;
+    }
+    //Initialize arrays
+    //Fill in. Loop through A and B and fill in initial value
+
+    //Now running time-stamp
+    begin = omp_get_wtime();
+    //Fill in. Loop and generate dot-product
+    #pragma omp parallel for reduction(+:dot_product)
+    for (int i = 0; i < N; i++)
+    {
+        dot_product += A[i]*B[i];
+    }
+    end = omp_get_wtime();
+
+    printf("The final dotproduct is %ld\n", dot_product);
+    printf("Time to execute was %lfs\n", end-begin);
+
+    return 0;
+}
